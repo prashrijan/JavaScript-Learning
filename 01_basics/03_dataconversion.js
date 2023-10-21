cd78ea244045e158150905babef8270c6a2a2274
prashrijan <141409513+prashrijan@users.noreply.github.com>
2023-10-21 03:57:20 +0000

    dataconversion

---
+++ 01_basics/03_dataconversion.js
@@ -0,0 +1,25 @@
+let score = "prashrijan"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// console.log(typeof NaN)
+
+// 33 => Number
+// "33" => NaN
+
+
+let loggedIn = -100
+
+let booleanIsLoggedIn = Boolean(loggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 => true
+// "" => false
+// "prashrijan" => true
