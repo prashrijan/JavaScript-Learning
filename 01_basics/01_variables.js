1b51aa266115e1f13a314b6055762caaf40a96ad
prashrijan <141409513+prashrijan@users.noreply.github.com>
2023-10-21 03:34:32 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const studentId = "K220390"
+let courseName = "BIT"
+var studentName = "Prashrijan"
+let fees;
+
+console.table([studentId, courseName, studentName, fees])
+
+// prefer not to use var because of issue in block scope and functional scope
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+//tc39 ecma standard
+
+// number
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value/empty 
+// undefined => 
+// symbol => unique
+
+
+// object
+
+// null is object 
+
+console.table([typeof null, typeof undefined, typeof "abcd", typeof true]);
