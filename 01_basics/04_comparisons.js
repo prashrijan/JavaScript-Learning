5e5784cd9b2d83dbfa3a2891c4225f6a0d366d64
prashrijan <141409513+prashrijan@users.noreply.github.com>
2023-10-31 04:48:51 +0000

    comaparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,15 @@
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+//avoid these kind of comparisions make your code understandable and clean
+
+// === strict check
