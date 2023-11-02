f4828ab7a7a1c9613179d2fbfad7241399045722
prashrijan <141409513+prashrijan@users.noreply.github.com>
2023-11-02 00:02:03 +0000

    important dataypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+const id = 1234;
+const myObject = {
+    name: "Prashrijan",
+    age: 20,
+    nationality: "Npali"
+}
+
+let myVar;
+
+const myFunction = function(){};
+
+// console.log(typeof myVar);
+
+const myObj = { };
+
+try{
+    myObj();
+}catch(error){
+    console.error("Error:", error);
+}
+
+console.log(typeof myObj);
