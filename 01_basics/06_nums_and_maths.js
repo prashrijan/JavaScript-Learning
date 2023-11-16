21798b35e55a90d8afdbdc4a6eaa6003e5172807
prashrijan <141409513+prashrijan@users.noreply.github.com>
2023-11-16 01:55:55 +0000

    zc

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -3,3 +3,7 @@ console.log(Math.random()*10);
 const max = 15
 const min = 5
 console.log(Math.floor(Math.random() * (max - min +1) + min))
+
+const otherNumbers = 1234.5677
+
+console.log(otherNumbers.toPrecision(4))
